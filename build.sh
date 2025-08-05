bf301879d7df390ef38caf67213efc09bf69f16b
sanchuanhehe <wyihe5220@gmail.com>
2025-08-05 14:27:35 +0800

    fix: Update native path variable to OHOS_NATIVE_PATH for consistency

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-native_path="/home/sanchuan/Documents/electron/src/ohos_sdk/openharmony/native"
-export CC="$native_path/llvm/bin/clang"
-export CXX="$native_path/llvm/bin/clang++"
-export LD="$native_path/llvm/bin/lld"
-export STRIP="$native_path/llvm/bin/llvm-strip"
-export RANLIB="$native_path/llvm/bin/llvm-ranlib"
-export OBJDUMP="$native_path/llvm/bin/llvm-objdump"
-export AR="$native_path/llvm/bin/llvm-ar"
-export NM="$native_path/llvm/bin/llvm-nm"
-export OBJCOPY="$native_path/llvm/bin/llvm-objcopy"
+OHOS_NATIVE_PATH="/home/sanchuan/Documents/electron/src/ohos_sdk/openharmony/native"
+export CC="$OHOS_NATIVE_PATH/llvm/bin/clang"
+export CXX="$OHOS_NATIVE_PATH/llvm/bin/clang++"
+export LD="$OHOS_NATIVE_PATH/llvm/bin/lld"
+export STRIP="$OHOS_NATIVE_PATH/llvm/bin/llvm-strip"
+export RANLIB="$OHOS_NATIVE_PATH/llvm/bin/llvm-ranlib"
+export OBJDUMP="$OHOS_NATIVE_PATH/llvm/bin/llvm-objdump"
+export AR="$OHOS_NATIVE_PATH/llvm/bin/llvm-ar"
+export NM="$OHOS_NATIVE_PATH/llvm/bin/llvm-nm"
+export OBJCOPY="$OHOS_NATIVE_PATH/llvm/bin/llvm-objcopy"
 
 
 # 配置交叉编译到 ARM64/AArch64
-export CFLAGS="-fPIC -D__MUSL__=1 --target=aarch64-linux-ohos --sysroot=$native_path/sysroot -I$native_path/sysroot/usr/include -I$native_path/sysroot/usr/include/aarch64-linux-ohos"
-export CXXFLAGS="-fPIC -D__MUSL__=1 --target=aarch64-linux-ohos --sysroot=$native_path/sysroot -I$native_path/sysroot/usr/include -I$native_path/sysroot/usr/include/aarch64-linux-ohos -I$native_path/llvm/include/c++/v1 -D_LIBCPP_PROVIDES_DEFAULT_RUNE_TABLE -D_LIBCPP_HAS_MUSL_LIBC -D_LIBCPP_HAS_NO_VENDOR_AVAILABILITY_ANNOTATIONS"
-export LDFLAGS="--target=aarch64-linux-ohos --sysroot=$native_path/sysroot -stdlib=libc++ -L$native_path/sysroot/usr/lib/aarch64-linux-ohos -L$native_path/llvm/lib -Wl,--gc-sections -Wl,-z,relro -Wl,-z,now"
-export SYSROOT="$native_path/sysroot"
+export CFLAGS="-fPIC -D__MUSL__=1 --target=aarch64-linux-ohos --sysroot=$OHOS_NATIVE_PATH/sysroot -I$OHOS_NATIVE_PATH/sysroot/usr/include -I$OHOS_NATIVE_PATH/sysroot/usr/include/aarch64-linux-ohos"
+export CXXFLAGS="-fPIC -D__MUSL__=1 --target=aarch64-linux-ohos --sysroot=$OHOS_NATIVE_PATH/sysroot -I$OHOS_NATIVE_PATH/sysroot/usr/include -I$OHOS_NATIVE_PATH/sysroot/usr/include/aarch64-linux-ohos -I$OHOS_NATIVE_PATH/llvm/include/c++/v1 -D_LIBCPP_PROVIDES_DEFAULT_RUNE_TABLE -D_LIBCPP_HAS_MUSL_LIBC -D_LIBCPP_HAS_NO_VENDOR_AVAILABILITY_ANNOTATIONS"
+export LDFLAGS="--target=aarch64-linux-ohos --sysroot=$OHOS_NATIVE_PATH/sysroot -stdlib=libc++ -L$OHOS_NATIVE_PATH/sysroot/usr/lib/aarch64-linux-ohos -L$OHOS_NATIVE_PATH/llvm/lib -Wl,--gc-sections -Wl,-z,relro -Wl,-z,now"
+export SYSROOT="$OHOS_NATIVE_PATH/sysroot"
 
 # 清理之前的构建
 rm -rf build node_modules
